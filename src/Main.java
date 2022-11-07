4c8ffcd328cd892f3cc57c3d8c84829fa90cbea7
Dmitriy Ogorodnikov <diminskiy_4791@mail.ru>
2022-11-07 15:58:04 +0300

    Пробел

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Привет Вася");
         System.out.println("Пока Вася");
+        System.out.println("__________");
     }
 }
