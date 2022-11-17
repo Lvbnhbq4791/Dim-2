7da7fd63d318750dacf70334b47f3cd9a59b8dd8
Dmitriy Ogorodnikov <diminskiy_4791@mail.ru>
2022-11-17 13:19:48 +0300

    HGhgkhg

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Привет Вася");
+//        System.out.println("\t"+"Привет Вася");
+//        System.out.println(" "+"Привет Вася");
+//        System.out.println("  "+"Привет Вася");
+//        System.out.println("   "+"Привет Вася");
+//        System.out.println("    "+"Привет Вася");
+        int g = 4 % 4;
+        int t = 2/3*2;
+        int u = t + g;
+
+        System.out.println (u);
     }
 }
